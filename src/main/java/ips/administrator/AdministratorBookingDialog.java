4bbc32b1e4cf62516294d9fe348922aedcde01f7
Tronerfull <torrentereguero11@gmail.com>
2016-10-04 22:04:06 +0200

    Adding button for center activities, created contructor for adminbookingdialog and linked to the button

--- src/main/java/ips/administrator/AdministratorBookingDialog.java
+++ src/main/java/ips/administrator/AdministratorBookingDialog.java
@@ -1,13 +1,29 @@
 package ips.administrator;
 
 import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.util.Date;
 
 /**
  * Created by nokutu on 03/10/2016.
  */
-public class AdministratorBookingDialog extends JDialog {
+public class AdministratorBookingDialog extends JDialog 
+{
 
-    public AdministratorBookingDialog() {
-        // TODO
+	private Date date;
+	private int hourStart;
+	private int hourEnd;
+
+	public AdministratorBookingDialog(JFrame owner, Date date, int hourStart, int hourEnd) 
+    {
+        // TODOpublic BookingDialog(JFrame owner, Date date, int hourStart, int hourEnd) {
+        super(owner);
+        setResizable(false);
+
+        this.date = date;
+        this.hourStart = hourStart;
+        this.hourEnd = hourEnd;
+
     }
 }
