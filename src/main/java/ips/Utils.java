c0a50c092f82cc530e2c239b7e272fe75db3373c
Jorge López <nokutu@openmailbox.org>
2016-10-10 20:58:55 +0200

    Añadidos métodos de utilidades

--- src/main/java/ips/Utils.java
+++ src/main/java/ips/Utils.java
@@ -5,6 +5,7 @@ import ips.database.Facility;
 import ips.database.FacilityBooking;
 
 import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -23,7 +24,7 @@ public class Utils {
     public static boolean isFacilityFree(Facility facility, Date timeStart, Date timeEnd) {
         List<FacilityBooking> bookings = Database.getInstance().getFacilityBookings();
         for (FacilityBooking fb : bookings) {
-            if (fb.getFacilityId() == facility.getFacilityId()) {
+            if (fb.getFacilityId() == facility.getFacilityId() && areSameDay(fb.getTimeStart(), timeStart)) {
                 if (timeStart.before(fb.getTimeStart()) && timeEnd.after(fb.getTimeStart())) {
                     return false;
                 }
@@ -34,5 +35,25 @@ public class Utils {
 
     public static Date addHourToDay(Date day, int hour) {
         return new Date(day.getTime() + hour * 1000 * 3600);
+    }
+
+    public static boolean areSameDay(Date a, Date b) {
+        Calendar calA = Calendar.getInstance();
+        Calendar calB = Calendar.getInstance();
+        calA.setTime(a);
+        calB.setTime(b);
+
+        return calA.get(Calendar.DAY_OF_YEAR) == calB.get(Calendar.DAY_OF_YEAR) &&
+                calA.get(Calendar.YEAR) == calB.get(Calendar.YEAR);
+    }
+
+    public static boolean areSameWeek(Date a, Date b) {
+        Calendar calA = Calendar.getInstance();
+        Calendar calB = Calendar.getInstance();
+        calA.setTime(a);
+        calB.setTime(b);
+
+        return calA.get(Calendar.WEEK_OF_YEAR) == calB.get(Calendar.WEEK_OF_YEAR) &&
+                calA.get(Calendar.YEAR) == calB.get(Calendar.YEAR);
     }
 }
