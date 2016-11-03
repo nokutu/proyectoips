28c2d63bc121bd8427d756146cf5d7f8dd2bd6f9
Jorge López <nokutu@openmailbox.org>
2016-11-03 09:23:50 +0100

    Arreglados los botones de gabri. Se ha cambiado el FlowLayout por un GridBagLayout.

--- src/main/java/ips/administrator/AdministratorMainScreen.java
+++ src/main/java/ips/administrator/AdministratorMainScreen.java
@@ -7,6 +7,9 @@ import ips.FeeUpdater;
 import ips.MainWindow;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 /**
  * Created by nokutu on 24/10/2016.
@@ -20,35 +23,51 @@ public class AdministratorMainScreen extends JPanel {
     public AdministratorMainScreen() {
         setLayout(new BorderLayout());
 
-        upperPanel = new JPanel();
+        upperPanel = new JPanel(new GridBagLayout());
         add(upperPanel, BorderLayout.NORTH);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(5, 10, 5, 10);
+        c.gridx = 0;
+        c.gridy = 0;
 
         // TODO añadir el panel de Tony a center y los listeners que llamen a setRightPanel
         
         JButton btnCurrentBooks = new JButton("Listar reservas en uso");
     	btnCurrentBooks.addActionListener(e ->  new CurrentViewDialog(MainWindow.getInstance()).setVisible(true));
-    	upperPanel.add(btnCurrentBooks);
+    	upperPanel.add(btnCurrentBooks, c);
+
+        c.gridx++;
     	
     	JButton btnPayDebts = new JButton("Pagar deuda en efectivo (no actual)");
     	btnPayDebts.addActionListener(e -> new PayDebtsDialog(MainWindow.getInstance()).setVisible(true));
-    	upperPanel.add(btnPayDebts);
-    	
+    	upperPanel.add(btnPayDebts, c);
+
+        c.gridx++;
+
     	JButton btnCurrentDebts = new JButton("Pagar deuda en efectivo (actual)");
     	btnCurrentDebts.addActionListener(e ->  new PayCurrentDebt(MainWindow.getInstance()).setVisible(true));
-    	upperPanel.add(btnCurrentDebts);
+    	upperPanel.add(btnCurrentDebts, c);
+
+        c.gridx = 0;
+        c.gridy++;
     	
     	JButton btnasistencia = new JButton("Mostrar faltas de asistencia");
     	btnasistencia.addActionListener(e ->  new AssistanceDialog().setVisible(true));
-    	upperPanel.add(btnasistencia);
+    	upperPanel.add(btnasistencia, c);
+
+        c.gridx++;
     	
     	JButton btnFeeUpdater = new JButton("Actualizar Tarifas");
 		btnFeeUpdater.addActionListener(e -> FeeUpdater.update());
-		upperPanel.add(btnFeeUpdater);
-    	
+		upperPanel.add(btnFeeUpdater, c);
+
+        c.gridx++;
     	
         JButton activitiesButton = new JButton("Ver actividades");
         activitiesButton.addActionListener(l -> new AdministratorActivitiesDialog().setVisible(true));
-        upperPanel.add(activitiesButton);
+        upperPanel.add(activitiesButton, c);
 
         setRightPanel(new AdministratorBookPanel());
     }
